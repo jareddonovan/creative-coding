bdcbe4108c1d1e18935e5660f49fe5e908cfe3c6
Jared Donovan <jared.donovan@gmail.com>
2019-02-20 18:18:05 +1000

    Delete some unused files.

--- docs/tutorials/tut09/images/fullsize/Learning Resources – DXB303_18se1 (QUT Blackboard)_files/LoadProactiveOverrideCss.jsp
+++
@@ -1,2 +0,0 @@
-
-  @import '/styles/qut/override-proactive.css'
--- docs/tutorials/tut09/images/fullsize/Learning Resources – DXB303_18se1 (QUT Blackboard)_files/mouse.js
+++
@@ -1,9 +0,0 @@
-eesy.define([], function() {
-  return {
-    x: 0,
-    y: 0,
-    lastelement: undefined,
-    lasturl: undefined,
-    hintmode: false
-  };
-});
